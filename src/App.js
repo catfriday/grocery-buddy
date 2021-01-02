553a05533612e0c940c588b6594e3c2e219896fe
Catrina Friday <cat.friday@gmail.com>
2021-01-02 16:35:50 -0500

    started alert

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({show:false, msg:'', type:''})
+  const [alert, setAlert] = useState({show:true, msg:'hello world', type:'success'})
 
   const handleSubmit = (e) =>{
     e.preventDefault()
@@ -27,7 +27,7 @@ function App() {
 
   return (<section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert />}
+      {alert.show && <Alert {...alert} />}
       <h3>grocery bud</h3>
       <div className='form-control'>
         <input className='text' className='grocery' placeholder='e.g. eggs' value={name} onChange={(e) => setName(e.target.value)}/>
@@ -36,10 +36,13 @@ function App() {
         </button>
       </div>
     </form>
+    {list.length > 0 &&(
+
     <div className='grocery-container'>
       <List items={list}/>
       <button className='clear-btn'>clear items</button>
     </div>
+    )}
   </section>)
 }
 
--- src/List.js
+++ src/List.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
+
+
 const List = ({items}) => {
 
   return(<div className='grocery-list'>
@@ -7,6 +9,14 @@ const List = ({items}) => {
       const {id, title} = item
       return <artical key={id} className='grocery-item'>
         <p className='title'>{title}</p>
+        <div className='btn-container'>
+          <button type='button' className='edit-btn'>
+            <FaEdit />
+          </button>
+          <button type='button' className='delete-btn'>
+            <FaTrash />
+          </button>
+        </div>
       </artical>
     })}
   </div>)
