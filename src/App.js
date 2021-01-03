f0944f40511f23019e748c502a2493f1aa11eda9
Catrina Friday <cat.friday@gmail.com>
2021-01-03 15:21:52 -0500

    added localStorage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
-function App() {
+const getLocalStaorage = () => {
+  let list = localStorage.getItem('list')
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }else{
+    return []
+  }
+}
 
+function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStaorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({show:false, msg:'', type:''})
@@ -25,6 +33,10 @@ function App() {
         }
         return item 
       }))
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed')
     }else{
       //show alert
       showAlert(true, 'success', 'item added to the list')
@@ -55,6 +67,10 @@ function App() {
     setEditID(id)
     setName(specificItem.title)
   }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
 
   return (<section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
